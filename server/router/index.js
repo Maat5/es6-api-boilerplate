8aec9d9c744f9efb4dcf39c437f9ad078aa243cc
Manuel Abrante <mabrante@maslowtech.com>
2016-12-05 16:54:25 -0400

    added middleware parser

--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const Controllers = require('../controllers');
+const ParserError = require('../middlewares/parser');
 
-let Router = (app) =>{
+let Router = (app) => {
+  // set lang to all routes and handle errors
+  app.use(ParserError);
   // user routes
   app.use('/user', Controllers.User);
+
+  //404 Route (ALWAYS Keep this as the last route)
+  app.use((req, res) =>{
+    res.status(404).send({ code: 404, success: false, key: true, message: req.ts('notFound') });
+  });
 };
 
 module.exports = Router;
