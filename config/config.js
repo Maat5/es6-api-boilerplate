6b103526c3d08957b018f37f50836c1e054a4e14
Manuel Abrante <mabrante@maslowtech.com>
2016-12-05 16:00:30 -0400

    create config file and mongodb connections config

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  port: 8080,
+  sessionToken: '0a6b944d-d2fb-46fc.rz9x8p9uqpvgzy',
+  db: {
+    password: "",
+    username: "",
+    host: "localhost",
+    database: "es6",
+    port: 27017
+  }
+}
---
+++ config/dbConnection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const uri = 'mongodb://' + config.db.host + '/' + config.db.database;
+
+mongoose.connect(uri, { user: config.db.username, password: config.db.password }, (err, res) => {
+  if (err) throw err;
+
+  console.log('connected to db' + uri);
+});
+
+module.exports = mongoose;
